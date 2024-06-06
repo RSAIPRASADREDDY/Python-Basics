a7a900d836893c6a6315a40c6496a6a215ed5ab3
Sai Reddy <rspr9676@gmail.com>
2024-06-06 14:57:59 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,18 @@
+##Variable is manage values and store something to use it insid ethe code for further use
+
+'''
+Variable creation
+1. always start with _(Underscore) or letter
+2. variables or case sensitive eg:- SAI and sai are twi diffirent
+3. variables can't use special characters
+4. variable can't start with numbers
+5. multi words variable can be seperated with _(Underscore)
+'''
+name="Sai"
+_name="Prasad"
+__name="Reddy"
+My_Name_is="Sai Prasad Reddy"
+##### 1jsf="reddy" #variable can't start with numbers
+a=input("Enter value a:-")
+b=input("Enter value b:-")
+print(a+b)  #here pring just concatinating not summing
