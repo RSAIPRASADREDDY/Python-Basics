57caf18129092dc9b66c50541dcab43a619cc09e
Sai Reddy <rspr9676@gmail.com>
2024-06-06 15:04:26 +0530

    Create PROJECT-1 BrandNameGenerator.py

---
+++ PROJECT-1 BrandNameGenerator.py
@@ -0,0 +1,14 @@
+#band name generator project
+
+####----PROJECT-1 METHOD
+"""
+print("Welcome to the band name generator")
+Name = input("What is the name of city you broughtup?\n")
+Pet = input("What is your pet name?\n")
+print (f"Your band name Could be {Name} {Pet}")
+"""
+
+######-----SAME PROJECT-2 Diffirent METHOD
+print(F'welcome to creating your own BRAND name')
+Name=input(f'Its your wish what would you like to name your BRAND \n')
+print(f'your brand name is "{Name}" \nCongratulations!!! Its Party Time')
