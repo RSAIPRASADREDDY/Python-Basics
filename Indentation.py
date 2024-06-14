26488193c4f6161573a16006ce5d2ce5a3533040
Sai Reddy <rspr9676@gmail.com>
2024-06-14 16:26:21 +0530

    Add files via upload

---
+++ Indentation.py
@@ -0,0 +1,19 @@
+'''
+indentaion is nothing but inside of specific code
+it contains 4 spaces equal to one indenation
+if you consider  a folder stracture, its actually indented
+
+
+'''
+def fun():
+    print()#this line is actually indented
+    a='10'
+    # if n='10':
+    #     print(a)
+
+
+
+
+
+
+
